bcbad3c015b563e427555b29e11789e3e05e1438
lasconic <lasconic@gmail.com>
2013-08-08 15:26:59 +0200

    add more trace

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <QtGui/QGuiApplication>
 #include "qtquick2applicationviewer.h"
+#include <QDebug>
+
+class MyQQuickView : public QQuickView
+      {
+      void resizeEvent(QResizeEvent* e) {
+            qDebug() << "resizeEvent";
+            QQuickView::resizeEvent(e);
+            }
+      };
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QQuickView view;
+    MyQQuickView view;
     view.setTitle("MuseScore Player");
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.setSource(QUrl("qrc:/qml/rotationTest/main.qml"));
--- qml/rotationTest/main.qml
+++ qml/rotationTest/main.qml
@@ -1,9 +1,13 @@
 import QtQuick 2.0
+import QtQuick.Window 2.0
 
 Rectangle {
     width: 360
     height: 360
     color: "red"
+
+    Screen.onOrientationChanged: {console.log("orientation changed !!!!!!!!!")}
+
     Text {
         text: qsTr("Hello World")
         anchors.centerIn: parent
